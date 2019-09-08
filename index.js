87aab8637c8067a4cb5201a1ffb7032ab5220ee3
Thulasi <atfornet@gmail.com>
2019-09-07 23:19:32 -0700

    Adding Global Context

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const toggleContent = () => {
-  console.log("clicked");
   let getContent = document.getElementById("content");
+  console.log(getContent);
   getContent.style.display === "none"
     ? (getContent.style.display = "block")
     : (getContent.style.display = "none");
+};
+
+const toggleGlobalContent = () => {
+  let getGlobalContent = document.getElementById("globalContent");
+  getGlobalContent.style.display === "none"
+    ? (getGlobalContent.style.display = "block")
+    : (getGlobalContent.style.display = "none");
 };
